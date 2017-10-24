e55bd7035cfbcf2311cbc19745e9205099f39dbc
Muza <artikovmuzaffar@gmail.com>
2017-10-24 18:03:45 +0500

    mdmsoft package deleted

--- basic/config/web.php
+++ basic/config/web.php
@@ -39,7 +39,9 @@ $config = [
             ],
         ],
         'db' => $db,
-
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager'
+        ],
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
