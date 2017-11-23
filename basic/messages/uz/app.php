36c1a5922493dd64f9ece5aa95f33fef8261b234
Muza <artikovmuzaffar@gmail.com>
2017-11-23 14:46:30 +0500

    University module Direction CRUD generated

--- basic/messages/uz/app.php
+++ basic/messages/uz/app.php
@@ -1,13 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Muza
- * Date: 04.11.17
- * Time: 14:42
- */
+
 return [
     'Create' => 'Yaratish',
     'Create Building' => 'Bino yaratish',
+    'Create Room' => 'Xona yaratish',
+    'Create Direction' => 'Yo\'nalish yaratish',
     'Create Faculty' => 'Fakultet yaratish',
     'Create Department' => 'Kafedra yaratish',
     'Update' => 'Tahrirlash',
@@ -15,4 +12,6 @@ return [
     'Faculties' => 'Fakultetlar',
     'Buildings' => 'Binolar',
     'Departments' => 'Kafedralar',
+    'Directions' => 'Yo\'nalishlar',
+    'Rooms' => 'Xonalar',
 ];
